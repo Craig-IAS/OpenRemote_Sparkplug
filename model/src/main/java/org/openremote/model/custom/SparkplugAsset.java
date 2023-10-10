c895419201aed7bc473bad7f16f6932d709aa56b
Craig-IAS <craigwebber@radixiot.com>
2023-10-10 06:55:50 -0600

    add extra attributes to sparkplug asset

--- model/src/main/java/org/openremote/model/custom/SparkplugAsset.java
+++ model/src/main/java/org/openremote/model/custom/SparkplugAsset.java
@@ -2,19 +2,32 @@ package org.openremote.model.custom;
 
 import org.openremote.model.asset.Asset;
 import org.openremote.model.asset.AssetDescriptor;
+import org.openremote.model.attribute.MetaItem;
+import org.openremote.model.attribute.MetaMap;
 import org.openremote.model.value.AttributeDescriptor;
 
 
 import jakarta.persistence.Entity;
+import org.openremote.model.value.MetaItemType;
+import org.openremote.model.value.ValueFormat;
 import org.openremote.model.value.ValueType;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Entity
 public class SparkplugAsset extends Asset<SparkplugAsset>   {
 
     public static final AttributeDescriptor<String> GROUP_ID = new AttributeDescriptor<>("GroupId", ValueType.TEXT);
-    public static final AttributeDescriptor<String> DEVICE_ID = new AttributeDescriptor<>("DeviceId", ValueType.TEXT).withOptional(true);
+    public static final AttributeDescriptor<String> DEVICE_ID = new AttributeDescriptor<>("DeviceId", ValueType.TEXT);
+    public static final AttributeDescriptor<Boolean> ONLINE = new AttributeDescriptor<>("Online", ValueType.BOOLEAN).withFormat(ValueFormat.BOOLEAN_ON_OFF());
+    public static final AttributeDescriptor<Boolean> NODE_CONTROL_REBOOT = new AttributeDescriptor<>("NodeControlReboot", ValueType.BOOLEAN).withFormat(ValueFormat.BOOLEAN_AS_PRESSED_RELEASED());
+    public static final AttributeDescriptor<Boolean> NODE_CONTROL_REBIRTH = new AttributeDescriptor<>("NodeControlRebirth", ValueType.BOOLEAN).withFormat(ValueFormat.BOOLEAN_AS_PRESSED_RELEASED());
+    public static final AttributeDescriptor<Boolean> NODE_CONTROL_NEXT_SERVER= new AttributeDescriptor<>("NodeControlNextServer", ValueType.BOOLEAN).withFormat(ValueFormat.BOOLEAN_AS_PRESSED_RELEASED());
+
+
+
+
 
 
     public static final AssetDescriptor<SparkplugAsset> DESCRIPTOR = new AssetDescriptor<>("sparkplug", null, SparkplugAsset.class);
@@ -37,6 +50,23 @@ public class SparkplugAsset extends Asset<SparkplugAsset>   {
     public Optional<String> getDeviceId() {
         return getAttributes().getValue(DEVICE_ID);
     }
+
+    public Optional<Boolean> getOnline() {
+        return getAttributes().getValue(ONLINE);
+    }
+
+    public Optional<Boolean> getNodeControlReboot() {
+        return getAttributes().getValue(NODE_CONTROL_REBOOT);
+    }
+
+    public Optional<Boolean> getNodeControlRebirth() {
+        return getAttributes().getValue(NODE_CONTROL_REBIRTH);
+    }
+
+    public Optional<Boolean> getNodeControlNextServer() {
+        return getAttributes().getValue(NODE_CONTROL_NEXT_SERVER);
+    }
+
 
 
 
